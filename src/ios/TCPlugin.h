50815b57b4de72d2c0f82d43a88aa570373b8977
Eric Bariaux <eric@openremote.org>
2014-12-15 11:10:02 +0100

    Those method declarations are part of the delegates protocol, no need to list them here

--- src/ios/TCPlugin.h
+++ src/ios/TCPlugin.h
@@ -10,23 +10,7 @@
 
 #import <Cordova/CDV.h>
 
-
 @interface TCPlugin : CDVPlugin <TCDeviceDelegate, TCConnectionDelegate> { }
-
-# pragma mark device delegate method
--(void)device:(TCDevice *)device didStopListeningForIncomingConnections:(NSError *)error;
--(void)device:(TCDevice*)device didReceiveIncomingConnection:(TCConnection*)connection;
--(void)device:(TCDevice *)device didReceivePresenceUpdate:(TCPresenceEvent *)presenceEvent;
--(void)deviceDidStartListeningForIncomingConnections:(TCDevice*)device;
--(void)deviceStatus:(CDVInvokedUrlCommand*)command;
-
-# pragma mark connection delegate methods
--(void)connection:(TCConnection*)connection didFailWithError:(NSError*)error;
--(void)connectionDidStartConnecting:(TCConnection*)connection;
--(void)connectionDidConnect:(TCConnection*)connection;
--(void)connectionDidDisconnect:(TCConnection*)connection;
--(void)connectionStatus:(CDVInvokedUrlCommand*)command;
--(void)connectionParameters:(CDVInvokedUrlCommand*)command;
 
 # pragma mark javascript mapper methods
 -(void)deviceSetup:(CDVInvokedUrlCommand*)command;
